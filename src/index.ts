886cf3baa5fec15bba5763f4f4a4667f348fc9dc
吴浩麟 <wuhaolin@meituan.com>
2020-08-06 11:24:34 +0800

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import axios from 'axios';
 import {registerMessageHandler} from "./registerMessageHandler";
+import {adapter} from "./adapter";
 
 registerMessageHandler();
+
+export const axiosInstance = axios.create({
+    adapter,
+})
